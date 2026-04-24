f20f8d5b416d54b52e5dc2d9f893e53a1d3c7d61
ldzhin2026 <levijinji@gmail.com>
2026-04-24 04:18:20 +0400

    Update main.py

--- main.py
+++ main.py
@@ -137,7 +137,16 @@ async def check_answer(callback: types.CallbackQuery, state: FSMContext):
             [InlineKeyboardButton(text="🟢 СТРИМЫ НА KICK", url="https://vtgori.pro/kick")],
             [InlineKeyboardButton(text="🎟️ РОЗЫГРЫШ", callback_data="start_giveaway")]
         ])
-        await callback.message.reply("✅ Пройдено!\nДобро пожаловать!\n\nЕсли вы выиграли на стриме...", reply_markup=kb, parse_mode="Markdown")
+        await callback.message.reply("✅ Пройдено!\n
+Привет дорогой друг!🤚\n\n
+Так как спонсором наших эфиров является DragonMoney, то 1000 руб. мы закидываем на игровой кабинет 
+(баланс будет в криптовалюте, не забудь поменять с рублей на крипту в настройках).\n\n
+Если ты выиграл в эфире и готов получить 1000 руб. на игровой кабинет, необходимо сделать следующее:\n\n
+✍️ 1. Зарегистрироваться по ссылке ниже в Dragon Money 
+(<a href='https://vtgori.pro/guidegive/'>инструкция здесь</a>)\n
+🎁 2. Нажать на кнопку \РОЗЫГРЫШ\ ниже и вставить ID аккаунта от Dragon Money\n
+🏆 3. Если ты выиграл в розыгрыше, тогда получишь 1000 руб. с возможностью вывода денег.\n\n
+☝️ Начисление возможно только нашему рефералу (тому кто зарегистрировался по нашей ссылке)", reply_markup=kb, parse_mode="Markdown")
         await state.clear()
         await callback.answer("Успех!")
     else:
