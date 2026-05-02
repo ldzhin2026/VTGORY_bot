60f4b836ae84bcb8867f43a2283b6a0b3aa05067
ldzhin2026 <levijinji@gmail.com>
2026-05-02 23:39:59 +0400

    Update main.py

--- main.py
+++ main.py
@@ -323,7 +323,7 @@ async def check_answer(callback: types.CallbackQuery, state: FSMContext):
             [InlineKeyboardButton(text="🎰 DRAGON MONEY", url="https://dm13.to/NtoO8")],
             [InlineKeyboardButton(text="🎟️ РОЗЫГРЫШ", callback_data="start_giveaway")],
             [InlineKeyboardButton(text="🛠️ МОДЕРАТОР", url="https://t.me/ModTolkogori")],
-            [InlineKeyboardButton(text="🟢 СТРИМЫ НА KICK", url="https://vtgori.pro/kick")]
+            [InlineKeyboardButton(text="🟢 СТРИМЫ НА KICK", url="https://kick.com/TOLKOGORI")]
         ])
         
         text = (
